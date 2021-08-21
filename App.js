be0d239c90d4d0316bac9ba862a539ac7363d511
RanggaGunarto <ranggagunarto@gmail.com>
2021-08-21 14:06:38 +0800

    Setting Up Fonts

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as Font from 'expo-font';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
 
+import Home from './components/Home'
+
+function getFonts(){
+  return Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito/Nunito-Bold.ttf')
+  });
+}
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if(fontsLoaded){
+    return (
+      <View>
+        <StatusBar style="auto" /> 
+        <Home />
+      </View>
+    );
+  } else {
+    return (
+      <AppLoading 
+        startAsync={ getFonts }
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.warn}
+      />
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
